cfee3782d3638f4574f079a1eac889c64c607a6c
Pomelo Kat <pomelokat@gmail.com>
2025-07-22 16:03:23 -0400

    added some tidying code to data loading function

--- src/load_data.py
+++ src/load_data.py
@@ -14,6 +14,26 @@ def parse_yelp_data() -> tuple[pd.DataFrame, pd.DataFrame]:
 def load_yelp_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     df_b = pd.read_csv("data/df_b_processed.csv")
     df_r = pd.read_csv("data/df_r_processed.csv")
+
+    df_b["categories"] = df_b["categories"].str.split(", ")
+    df_b["postal_code"] = df_b["postal_code"].astype(pd.Int64Dtype())
+    df_b["is_open"] = df_b["is_open"] > 0
+
+    def parse_hours(hours: str | float) -> dict[str, tuple[np.datetime64, np.datetime64]] | float:
+        if isinstance(hours, float):
+            return hours
+        hours = eval(hours)
+        epoch = np.datetime64("1970-01-01", "D")
+        def parse_hours_time(interval: str) -> tuple[np.datetime64, np.datetime64]:
+            a, b = interval.split("-")
+            a_h, a_m = a.split(":")
+            b_h, b_m = b.split(":")
+            return epoch + np.timedelta64(int(a_h), "h") + np.timedelta64(int(a_m), "m"), epoch + np.timedelta64(int(b_h), "h") + np.timedelta64(int(b_m), "m")
+        return {day: parse_hours_time(h) for day, h in hours.items()}
+
+    df_b["hours"] = df_b["hours"].apply(parse_hours)
+    df_r["date"] = pd.to_datetime(df_r["date"])
+
     return df_b, df_r
 
 if __name__ == "__main__":
